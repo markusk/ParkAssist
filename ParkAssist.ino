c4d9a5de24cd95c2abca43a2dadf6f1a528cc9d4
Markus Knapp <github@direcs.de>
2022-05-25 00:11:04 +0200

    replaced delay with millis much faster reaction now!

--- ParkAssist.ino
+++ ParkAssist.ino
@@ -11,6 +11,12 @@ const int blinkFactor = 10;
 // at this distance (or lower) the LED stays on - to stop the car
 const int parkHereDistance = 50;
 
+// needed for a better delay
+unsigned long previousMillis = 0;
+// ledState used to set the LED
+int ledState = LOW;
+
+
 
 
 
@@ -31,31 +37,29 @@ void loop()
 {
   // trigger ultrasonic sensor now
   measure();
- 
-  if (duration == 0)
+
+  // check time for 'delay'
+  unsigned long currentMillis = millis();
+
+  // blink LED depending on the distance
+  if (currentMillis - previousMillis >= (distance * blinkFactor))
   {
-    //Serial.println("Warning: no pulse from sensor");
-    // set for blinking
-    distance = 1;
-  } 
-  else
-  {
-    distance = duration / 29.1 / 2;
-    //Serial.print("Distance:  ");
-    //Serial.print(distance);
-    //Serial.println(" cm");
+    // save the last time you blinked the LED
+    previousMillis = currentMillis;
+
+    // if the LED is off turn it on and vice-versa:
+    if (ledState == LOW)
+    {
+      ledState = HIGH;
+    }
+    else
+    {
+      ledState = LOW;
+    }
+
+    // set the LED with the ledState of the variable:
+    digitalWrite(LED_BUILTIN, ledState);
   }
-
-
-  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_BUILTIN, HIGH);
-  // wait depending on the measured distance
-  delay(distance * blinkFactor);
-
-  // turn the LED off by making the voltage LOW
-  digitalWrite(LED_BUILTIN, LOW);
-  // wait depending on the measured distance
-  delay(distance * blinkFactor);
 }
 
 
@@ -69,4 +73,19 @@ void measure(void)
   
   // convert the time into a distance
   duration = pulseIn(ECHO_PIN, HIGH);
+
+  // sensor okay?
+  if (duration == 0)
+  {
+    //Serial.println("Warning: no pulse from sensor");
+    // set for blinking
+    distance = 1;
+  } 
+  else
+  {
+    distance = duration / 29.1 / 2;
+    //Serial.print("Distance:  ");
+    //Serial.print(distance);
+    //Serial.println(" cm");
+  }
 }
